4ee5c77d5a198decfd298572514228ccb012fc64
bigibas123 <bigibas123@users.noreply.github.com>
2017-10-06 19:06:57 +0200

    Removed the bukkit from the working tree

--- wd/start.bat
+++
@@ -1,2 +0,0 @@
-cp %CD%\..\target\ServerChangeGui-1.2.jar %CD%\plugins\ServerChangeGui-1.2.jar
-"%CD%\..\..\..\Javas\Java\jdk1.8.0_73\bin\java.exe" -Dfile.encoding=UTF-8 -server -XX:+TieredCompilation -jar -XX:CompileThreshold=10 -XX:+UseG1GC -XX:MaxGCPauseMillis=1000 -Xmx256M -Xms256M spigot-1.8.8.jar nogui
--- wd/start.sh
+++
@@ -1,1 +0,0 @@
-java -Dfile.encoding=UTF-8 -Djline.terminal=jline.unix -jar -Xmx256M -Xms256M forge-1.8-11.14.4.1563-universal.jar
