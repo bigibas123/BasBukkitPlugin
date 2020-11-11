448f8b6778fce07df4c5cfc9efc1163560c66834
Bas Dingemans <478244@student.saxion.nl>
2020-11-11 14:17:06 +0100

    moved general permission check to main command method

--- src/main/java/com/github/bigibas123/serverchangegui/SCGCommand.java
+++ src/main/java/com/github/bigibas123/serverchangegui/SCGCommand.java
@@ -41,7 +41,12 @@ public class SCGCommand implements CommandExecutor, TabCompleter {
 		if (args.length > 0) {
 			String cmd = args[0].toLowerCase();
 			if (subcommands.containsKey(cmd)) {
-				success = subcommands.get(cmd).apply(sender, args);
+				if (Util.hasPermission(sender, "SCG." + cmd)) {
+					success = subcommands.get(cmd).apply(sender, args);
+				} else {
+					ChatHelper.quickNoPermission(sender, "SCG." + cmd);
+					success = false;
+				}
 			} else {
 				new ChatHelper(sender, SEVERE).append("Invalid subcommand").send();
 				success = false;
@@ -64,70 +69,55 @@ public class SCGCommand implements CommandExecutor, TabCompleter {
 	}
 
 	private boolean reload(CommandSender sender, String[] args) {
-		if (Util.hasPermission(sender, "SCG.reload")) {
-			plugin.getMenu().reload();
-		} else {
-			ChatHelper.quickNoPermission(sender, "SCG.reload");
-		}
+		plugin.getMenu().reload();
 		return true;
 	}
 
 	private boolean save(CommandSender sender, String[] args) {
-		boolean success;
-		if (Util.hasPermission(sender, "SCG.save")) {
-			plugin.getMenu().save();
-			plugin.getConfigHelper().save();
-		} else {
-			ChatHelper.quickNoPermission(sender, "SCG.save");
-		}
-		success = true;
-		return success;
+		plugin.getMenu().save();
+		plugin.getConfigHelper().save();
+		return true;
 	}
 
 	private boolean setItem(CommandSender sender, String[] args) {
 		boolean success;
-		if (Util.hasPermission(sender, "SCG.setitem")) {
-			if (sender instanceof Player) {
-				if (args.length > 1) {
-					if (Util.hasPermission(sender, "SCG.setitem." + args[1])) {
-						@NotNull ItemStack original = ((Player) sender).getInventory().getItemInMainHand().clone();
-						ItemStack stack = ItemStackBuilder.of(original)
-								.transformMeta(itemMeta -> {
-									if (itemMeta.hasDisplayName()) {
-										String name = itemMeta.getDisplayName();
-										name = Util.replaceColorCodes(name);
-										itemMeta.setDisplayName(name);
-									}
-									List<String> lore = itemMeta.getLore();
-									if (lore != null) {
-										lore.replaceAll(Util::replaceColorCodes);
-										itemMeta.setLore(lore);
-									}
-									itemMeta.addItemFlags(original.getItemMeta() != null ? original.getItemMeta().getItemFlags().toArray(new ItemFlag[0]) : new ItemFlag[0]);
-								}).build();
-						success = plugin.getMenu().setItem(args[1], stack);
-						if (success) {
-							new ChatHelper(sender, GOOD)
-									.append("Server item of: ").append(HIGHLIGHT, args[1])
-									.append(GOOD, " set to: ").append(HIGHLIGHT, stack.getType().name()).send();
-						} else {
-							new ChatHelper(sender, BAD).append("Server: ").append(HIGHLIGHT, args[1]).append(BAD, " not found").newLine(PS)
-									.append("Please use one of: ").append(Arrays.toString(plugin.getMenu().getServers().toArray())).send();
-						}
+		if (sender instanceof Player) {
+			if (args.length > 1) {
+				if (Util.hasPermission(sender, "SCG.setitem." + args[1])) {
+					@NotNull ItemStack original = ((Player) sender).getInventory().getItemInMainHand().clone();
+					ItemStack stack = ItemStackBuilder.of(original)
+							.transformMeta(itemMeta -> {
+								if (itemMeta.hasDisplayName()) {
+									String name = itemMeta.getDisplayName();
+									name = Util.replaceColorCodes(name);
+									itemMeta.setDisplayName(name);
+								}
+								List<String> lore = itemMeta.getLore();
+								if (lore != null) {
+									lore.replaceAll(Util::replaceColorCodes);
+									itemMeta.setLore(lore);
+								}
+								itemMeta.addItemFlags(original.getItemMeta() != null ? original.getItemMeta().getItemFlags().toArray(new ItemFlag[0]) : new ItemFlag[0]);
+							}).build();
+					success = plugin.getMenu().setItem(args[1], stack);
+					if (success) {
+						new ChatHelper(sender, GOOD)
+								.append("Server item of: ").append(HIGHLIGHT, args[1])
+								.append(GOOD, " set to: ").append(HIGHLIGHT, stack.getType().name()).send();
 					} else {
-						ChatHelper.quickNoPermission(sender, "SCG.setitem." + args[1]);
-						success = true;
+						new ChatHelper(sender, BAD).append("Server: ").append(HIGHLIGHT, args[1]).append(BAD, " not found").newLine(PS)
+								.append("Please use one of: ").append(Arrays.toString(plugin.getMenu().getServers().toArray())).send();
 					}
 				} else {
-					success = false;
+					ChatHelper.quickNoPermission(sender, "SCG.setitem." + args[1]);
+					success = true;
 				}
 			} else {
-				new ChatHelper(sender, SEVERE).append("This command requires arguments or to be run as a player").newLine(PS)
-						.append("You are:").append(sender.getClass().getSimpleName()).send();
-				success = true;
+				success = false;
 			}
 		} else {
-			ChatHelper.quickNoPermission(sender, "SCG.setitem");
+			new ChatHelper(sender, SEVERE).append("This command requires arguments or to be run as a player").newLine(PS)
+					.append("You are:").append(sender.getClass().getSimpleName()).send();
 			success = true;
 		}
 		return success;
@@ -135,36 +125,31 @@ public class SCGCommand implements CommandExecutor, TabCompleter {
 
 	private boolean setSlot(CommandSender sender, String[] args) {
 		boolean success;
-		if (Util.hasPermission(sender, "SCG.setslot")) {
-			if (args.length > 2) {
-				if (Util.hasPermission(sender, "SCG.setslot." + args[1])) {
-					int slot = Integer.parseInt(args[2]);
-					if (plugin.getMenu().isSlotFree(slot)) {
-						success = plugin.getMenu().setSlot(args[1], slot);
-						if (success) {
-							new ChatHelper(sender, GOOD).append("Set slot of: ").append(HIGHLIGHT, args[1])
-									.append(GOOD, " to ").append(HIGHLIGHT, Integer.valueOf(slot).toString()).send();
-						} else {
-							new ChatHelper(sender, BAD).append("Server: ").append(HIGHLIGHT, args[1]).append(BAD, " not found").newLine(PS)
-									.append("Please use one of: ").append(Arrays.toString(plugin.getMenu().getServers().toArray())).send();
-						}
+		if (args.length > 2) {
+			if (Util.hasPermission(sender, "SCG.setslot." + args[1])) {
+				int slot = Integer.parseInt(args[2]);
+				if (plugin.getMenu().isSlotFree(slot)) {
+					success = plugin.getMenu().setSlot(args[1], slot);
+					if (success) {
+						new ChatHelper(sender, GOOD).append("Set slot of: ").append(HIGHLIGHT, args[1])
+								.append(GOOD, " to ").append(HIGHLIGHT, Integer.valueOf(slot).toString()).send();
 					} else {
-						new ChatHelper(sender, BAD).append("Slot: ").append(HIGHLIGHT, Integer.toString(slot)).append(BAD, " already taken").newLine(PS)
-								.append("Please choose one that is not: ").append(Arrays.toString(plugin.getMenu().getTakenSlots())).send();
-						success = true;
+						new ChatHelper(sender, BAD).append("Server: ").append(HIGHLIGHT, args[1]).append(BAD, " not found").newLine(PS)
+								.append("Please use one of: ").append(Arrays.toString(plugin.getMenu().getServers().toArray())).send();
 					}
 				} else {
-					ChatHelper.quickNoPermission(sender, "SCG.setslot." + args[1]);
+					new ChatHelper(sender, BAD).append("Slot: ").append(HIGHLIGHT, Integer.toString(slot)).append(BAD, " already taken").newLine(PS)
+							.append("Please choose one that is not: ").append(Arrays.toString(plugin.getMenu().getTakenSlots())).send();
 					success = true;
 				}
 			} else {
-				new ChatHelper(sender, BAD).append("Not enough arguments").newLine(PS)
-						.append("/scg setslot <server> <slot>").send();
-				success = false;
+				ChatHelper.quickNoPermission(sender, "SCG.setslot." + args[1]);
+				success = true;
 			}
 		} else {
-			ChatHelper.quickNoPermission(sender, "SCG.setslot");
-			success = true;
+			new ChatHelper(sender, BAD).append("Not enough arguments").newLine(PS)
+					.append("/scg setslot <server> <slot>").send();
+			success = false;
 		}
 		return success;
 	}
@@ -178,8 +163,8 @@ public class SCGCommand implements CommandExecutor, TabCompleter {
 	public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
 		ArrayList<String> suggestions = new ArrayList<>();
 		if (args.length == 1) {
-			for(String cmd: subcommands.keySet()){
-				if(cmd.startsWith(args[0]) && Util.hasPermission(sender,"SCG."+cmd)){
+			for (String cmd : subcommands.keySet()) {
+				if (cmd.startsWith(args[0]) && Util.hasPermission(sender, "SCG." + cmd)) {
 					suggestions.add(cmd);
 				}
 			}
